a47e4088f358c75bb256ad32556b2fb938805741
Sherman-1 <herman.simon.lm@gmail.com>
2021-12-29 23:10:56 +0100

    Commit un peu crade où je mets la bonne version de ORF_Finder dans la branche utils

--- ORF_Finder.c
+++ ORF_Finder.c
@@ -5,9 +5,10 @@
 
 int main() {
 
-    char sequence[100] = "AAAAUG_______________AUU_AUG________________________AUUU______";
+    char sequence[100] = "AUGAA";
     int i, k;
-    int n = 0;
+    int number_ORF = 0;
+    int longest_ORF;
     int boolean_stop;
 
 
@@ -19,14 +20,14 @@ int main() {
 
     } OpenReadingFrame;
 
-    OpenReadingFrame ORF_trouves[100] = {0}; 
+    OpenReadingFrame ORF_trouves[100] = {0};
 
     for ( i = 0; i < 42; i++) {
 
-        if ( sequence[i] == 'A' && sequence[i+1] == 'U' && sequence[i+2] == 'G' ) { 
+        if ( sequence[i] == 'A' && sequence[i+1] == 'T' && sequence[i+2] == 'G' ) {
 
-            ORF_trouves[n].start = i;
-            printf("Un codon start a ete trouve en position %d\n",ORF_trouves[n].start);
+            ORF_trouves[number_ORF].start = i;
+            printf("Un codon start a ete trouve en position %d\n",ORF_trouves[number_ORF].start+1);
             boolean_stop = 0;
             k = i+3;
 
@@ -34,21 +35,73 @@ int main() {
 
                 if ( sequence[k] && sequence[k+1] && sequence[k+2]) {
 
-                    printf("%d",k);
+                    if ( sequence[k] == 'T' && sequence[k+1] == 'G' && sequence[k+2] == 'A') {
 
-                    if ( sequence[k] == 'A' && sequence[k+1] == 'U' && sequence[k+2] == 'U') {
+                        ORF_trouves[number_ORF].stop = k;
+                        printf("Un codon stop a ete trouve en position %d\n",ORF_trouves[number_ORF].stop+1);
 
-                        ORF_trouves[n].stop = k;
-                        printf("Un codon stop a ete trouve en position %d\n",ORF_trouves[n].stop);
+                        ORF_trouves[number_ORF].length = (ORF_trouves[number_ORF].stop) - ORF_trouves[number_ORF].start+3;
 
-                        ORF_trouves[n].length = (ORF_trouves[n].stop+3) - ORF_trouves[n].start;
+                        printf("Une ORF de taille %d a ete trouvee\n",ORF_trouves[number_ORF].length);
 
-                        printf("Une ORF de taille %d a ete trouvee\n",ORF_trouves[n].length);
+
+                        if ( ORF_trouves[number_ORF].length > ORF_trouves[number_ORF-1].length) {
+
+                            longest_ORF = number_ORF;
+                        }
+
+
+                        number_ORF += 1;
                         boolean_stop = 1;
 
-                        n += 1;
-                        
+
+
                     }
+
+                    if ( sequence[k] == 'T' && sequence[k+1] == 'A' && sequence[k+2] == 'G') {
+
+                        ORF_trouves[number_ORF].stop = k;
+                        printf("Un codon stop a ete trouve en position %d\n",ORF_trouves[number_ORF].stop+1);
+
+                        ORF_trouves[number_ORF].length = (ORF_trouves[number_ORF].stop) - ORF_trouves[number_ORF].start+3;
+
+                        printf("Une ORF de taille %d a ete trouvee\n",ORF_trouves[number_ORF].length);
+
+
+                        if ( ORF_trouves[number_ORF].length > ORF_trouves[number_ORF-1].length) {
+
+                            longest_ORF = number_ORF;
+                        }
+
+
+                        number_ORF += 1;
+                        boolean_stop = 1;
+
+
+
+                    }
+
+                    if ( sequence[k] == 'T' && sequence[k+1] == 'A' && sequence[k+2] == 'A') {
+
+                        ORF_trouves[number_ORF].stop = k;
+                        printf("Un codon stop a ete trouve en position %d\n",ORF_trouves[number_ORF].stop+1);
+
+                        ORF_trouves[number_ORF].length = (ORF_trouves[number_ORF].stop) - ORF_trouves[number_ORF].start+3;
+
+                        printf("Une ORF de taille %d a ete trouvee\n",ORF_trouves[number_ORF].length);
+
+
+                        if ( ORF_trouves[number_ORF].length > ORF_trouves[number_ORF-1].length) {
+
+                            longest_ORF = number_ORF;
+                        }
+
+
+                        number_ORF += 1;
+                        boolean_stop = 1;
+
+                    }
+
                     k += 3;
 
                 }
@@ -57,14 +110,13 @@ int main() {
 
                     break;
                 }
-                
+
             } while ( boolean_stop == 0 );
 
         }
     }
 
-    printf("%d\n",n);
-    printf("Le plus grand reading frame est de taille %d\n",ORF_trouves[n-1].length);
+    printf("Le plus grand reading frame est de taille %d\n",ORF_trouves[longest_ORF].length);
     return 0;
-    
+
 }
