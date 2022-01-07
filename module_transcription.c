50cca4e7baace0c16618a847098cc594f027ad10
DRake_SY <maxime.criado@universite-paris-saclay.fr>
2022-01-07 14:47:57 +0100

    mise en place : get_path_user + save_sequence

--- module_transcription.c
+++ module_transcription.c
@@ -9,9 +9,8 @@ void transcription(){
   //on initialise la chaine de caractère contenant la voie du fichier
   char path_input[PATH_INPUT_MAX_SIZE];
 
-  printf("quel est le nom du fichier que vous voulez lire \n");
-  scanf("%s", path_input); //nom du fichier test = test_fasta (fichier cours sans la première ligne)
-  //on ne peut mettre que le nom du fichier et non tout le chemin
+  get_path_user(path_input);
+
   printf("\n");
 
   //on cherche la taille de la séquence
@@ -24,6 +23,7 @@ void transcription(){
   char sequence[taille_fasta];
   extract_sequence(path_input, sequence);
 
+  printf("%s\n", sequence);
 
   // Booléens de vérification :
   // pour la taille(%3) + codon START au début
@@ -43,12 +43,10 @@ void transcription(){
   }
 
 
-  FILE* op= fopen("séquence_transcrite.txt","w");
-
   //CHECK de la taille de la séquence
   int taille_sequence=strlen(sequence);
 
-  if(taille_sequence%3==0){
+  if((strlen(sequence)-1)%3==0){
     printf("La taille de la séquence est bonne \n");
     taille[0] = 'T'; // Vrais si la taille est divisible par 3
   }
@@ -56,40 +54,45 @@ void transcription(){
     printf("La taille de la séquence n'est pas bonne \n");
   }
 
-  int compteur = 0;
+  char sequence_transcrite[taille_sequence];
 
   //si les conditions sont remplis on fait la transcription
   // on réalise le changement de base
-	
+
   if(codant[0]=='T' && taille[0]=='T'){
     for (i=0;i<taille_sequence;i++){
-      if (sequence[i]=='T'){
-        fprintf(op,"A");
-        compteur = compteur +1;
+      if (sequence[i]=='\0'){
+        i++;
+      }
+      else if (sequence[i]=='T'){
+        sequence_transcrite[i]='A';
+
       }
       else if (sequence[i]=='A'){
-        fprintf(op,"U");
-        compteur = compteur +1;
+        sequence_transcrite[i]='U';
+
       }
       else if (sequence[i]=='G'){
-        fprintf(op,"C");
-        compteur = compteur +1;
-      }
-      else if (sequence[i]=='C'){
-        fprintf(op,"G");
-        compteur = compteur +1;
-      }
+        sequence_transcrite[i]='C';
 
-      if (compteur == 80){
-        fprintf(op,"\n");
-        compteur = 0;
+      }
+      else{
+        sequence_transcrite[i]='G';
       }
     }
-  } //on obtient dans un nouveaux fichier le brin transcris d'ARN
+
+
+  printf("%s \n", sequence_transcrite);
+
+  char path_output[PATH_INPUT_MAX_SIZE];
+  printf("\nVeuillez entrer le nom du fichier dans lequel s'écrira la séquence transcrite\n");
+  scanf("%s",path_output);
+  save_sequence(path_output,sequence_transcrite);
+  }
+   //on obtient dans un nouveaux fichier le brin transcris d'ARN
   else{
     printf("Redonnez une séquence correcte \n");
     // si la séquence est pas bonne = message d'erreur et on doit recommencer le programme
     // a mettre sous un while pour ne pas avoir a redemarrer le programme
   }
-
 }
